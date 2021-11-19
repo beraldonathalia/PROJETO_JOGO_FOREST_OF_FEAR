21207171944e61e57d2c54cfb04f9fddfd3ff591
beraldonathalia <beraldonathalia@outlook.com>
2021-11-18 22:35:49 -0300

    Primeiro commit

---
+++ Game_Forest_Of_Fear.py
@@ -0,0 +1,246 @@
+from os import system, name
+
+personagem_1 = 'Princesa Daiana'
+personagem_2 = 'Cavaleiro Brulon'
+personagem_3 = 'Feiteceiro Tozart'
+
+# ------------------------ PERSONAGENS SECUNDÁRIOS - PRINCESA DAIANA ---------------------------
+personagem_Unicornio = 'Unicórnio'
+personagem_Fada = 'Fada'
+personagem_Duende = 'Duende'
+
+# ------------------------ PERSONAGENS SECUNDÁRIOS - CAVALHEIRO BRULON ---------------------------
+personagem_Sereia = 'Sereia'
+personagem_Leão = 'Leão'
+personagem_Dragão = 'Dragão'
+
+# ------------------------ PERSONAGENS SECUNDÁRIOS - FEITICEIRO TOZART ---------------------------
+personagem_Portal = 'Portal'
+personagem_Voz = 'Voz'
+personagem_SerImortal = 'SerImortal'
+
+personagem = 4
+personagensSegundarios = 4 
+
+_ = system('cls')
+
+# função personagem_1
+def estoriaPersonagem1():
+    # ------------------------ Cena 1 - Unicórnio ---------------------------
+
+    print('\nNesta manhã nublada na floresta do medo, ', personagem_1, ' decide caminhar.')
+    print('\nCaminhando sem rumo, avistou um belo unicórnio.')
+    print('\nOlá, Sr. Unicórnio. O que faz nessa manhã pela floresta?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nBom dia,', personagem_1, 'acordei cedo, ouvi rumores que no reino das bruxas está acordado.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nNão será só rumores, Sr. Unicórnio? Já faz muito tempo que não temos e vemos alguma movimentação?!')
+    input('\nPressione uma tecla para continuar.')
+    print('\nPois é, mas é o que todos na floresta estão dizendo. Você é corajosa. Não quer ir até lá e ver?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nSe ir até lá, provará e será considerada a Princesa mais corajosa da floresta.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nNão estou certa disso, ainda. Vou continuar caminhando. Obrigada, Sr. Unicórnio.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nEnquanto ', personagem_1, ' caminha pela floresta, uma forte chuva se inicia.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nCorrendo a procura de um abrigo, avista uma fada encantada (conhecida por falar verdades e mentiras sobre a floresta) presa no arbusto. \nPreocupada com o que vê, decide parar.')
+
+    # ----------------------- Cena 2 - A fada ---------------------------------
+
+    print('\nFada, você está presa. O que aconteceu?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nAinda bem que você chegou, ', personagem_1, '. No caminho, indo para o lago encantado, \no gavião me perseguiu e acabei caindo aqui.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nÔ céus, sua asinha está quebrada, o que faremos?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nEssa não, e agora? Não poderei voar até o vale encantado, você pode me levar?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nClaro que sim, fada. Vou te tirar com cuidado!')
+    input('\nPressione uma tecla para continuar.')
+    print('\n',personagem_1, 'você terá que ter cuidado, pois o caminho até o vale encantado está perigoso. \nAlgumas fadas não gostaram que eu sai e fui andar pela floresta.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nE porque você saiu?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nElas querem me prender e eu fui ver como anda a floresta.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nVocê pode me levar de volta até o lago?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nVamos para o vale mesmo o caminho estando perigoso, Fada?')
+    input('\nPressione uma tecla para continuar.')
+    print('\nVocê escolhe,'',', personagem_1,'!')
+
+    # ----------------------- Cena 3 - O duende ---------------------------------
+
+    print('Continuando a caminhada...', personagem_1, 'acaba entrando na terra dos dragões.')
+    input('\nPressione uma tecla para continuar.')
+    print('\nA terra dos dragões é conhecida por ser dominada por seres astutos e ferozes.')
+    input('\nPressione um tecla para continuar')
+    print('\nEnquanto caminha,', personagem_1, 'esbarra em um duente.')
+    input('\nPressione um tecla para continuar')
+    print('\nPequeno duende, estou perdida, que lugar é esse? Você pode me ajudar?')
+    input('\nPressione um tecla para continuar')
+    print('\n',personagem_1, 'você está na terra dos dragões. Como parou aqui?')
+    input('\nPressione um tecla para continuar')
+    print('\nAconteceram algumas surpresas no caminho e acabei não percebendo. Me ajuda!')
+    input('\nPressione um tecla para continuar')
+    print('\nTodos sabem que para sair da terra dos dragões, tem apenas uma opção.')
+    input('\nPressione um tecla para continuar')
+    print('\nPor favor, então me diga!')
+    input('\nPressione um tecla para continuar')
+    print('\nEntrar no poço de zafir e sair!')
+    input('\nPressione um tecla para continuar')
+    print('\nVocê escolhe'',',personagem_1, '!')
+    print('\nA partir dos 3 cenários apresentados, qual você deseja prosseguir?',personagem_Unicornio,',',personagem_Fada,'ou', personagem_Duende, '?')
+
+# função personagem_2
+def estoriaPersonagem2():
+    # ------------------------ Cena 1 - Canto da sereia ---------------------------
+
+    print('\nNa calada do anoitecer,', personagem_2, 'sai pela floresta, para fazer a vigilância.')
+    input('\nPressione um tecla para continuar')
+    print('\nEssa floresta é conhecida por muitas surpresas...enquanto caminha com seu cavalo, ouve um lindo canto da sereia.')
+    input('\nPressione um tecla para continuar')
+    print('\nA seria é conhecida por envolver qualquer habitante da floresta. Uma vez que entra, ninguém consegue sair.')
+    input('\nPressione um tecla para continuar')
+    print('\nO cavalheiro sem prestar atenção, acaba indo ao encontro da linda sereia.')
+    input('\nPressione um tecla para continuar')
+    print('\nOlá, belo cavalheiro, que prazer te receber aqui!')
+    input('\nPressione um tecla para continuar')
+    print('\nComo eu cheguei aqui?')
+    input('\nPressione um tecla para continuar')
+    print('\nMeu canto, te trouxe até mim!')
+    input('\nPressione um tecla para continuar')
+    print('\nMe deixe sair, não quero ficar preso aqui!')
+    input('\nPressione um tecla para continuar')
+    print('\nVocê poderá sair daqui quando quiser. Olhe para frente, você pode tentar sair por está porta.')
+    input('\nPressione um tecla para continuar')
+    print('\n1. Atrás desta porta, tem um leão que está faminto, pois não come à 1 ano.')
+    input('\nPressione um tecla para continuar')
+    print('\nVocê escolhe', personagem_2, '!')
+    input('\nPressione um tecla para continuar')
+    
+    # ------------------------ Cena 2 - Leão ---------------------------
+
+    print('\nApós, passar pela sereia,', personagem_2, 'se depara com uma cobra enrolada no leão. O leão aflito, olha com olhar de ajuda para o cavalheiro.')
+    input('\nPressione um tecla para continuar')
+    print('\nCavalheiro me ajude, por favor. Mate essa cobra!')
+    input('\nPressione um tecla para continuar')
+    print('\nVocê escolhe,', personagem_2, '!')
+    input('\nPressione um tecla para continuar')
+
+    # ------------------------ Cena 3 - Dragão ---------------------------
+
+    print('\nCaminhando pela floresta,', personagem_2, 'vê o poderoso dragão voando ferozmente.')
+    input('\nPressione um tecla para continuar')
+    print('\nEsse dragão é conhecido na floresta por ser irredutível. Ele só da uma chance de sobreviver.')
+    input('\nPressione um tecla para continuar')
+    print('\nCavalheiro, Cavalheiro!! Não acredito que te encontrei hahahahahaha')
+    input('\nPressione um tecla para continuar')
+    print('\nInfelizmente, dragão. Depois de tantos anos!')
+    input('\nPressione um tecla para continuar')
+    print('\nSabe que desta vez não poderá fugir de mim.')
+    input('\nPressione um tecla para continuar')
+    print('\nNão fugirei, irei enfrentar.')
+    input('\nPressione um tecla para continuar')
+    print('\nAssim que eu gosto hahaha. Você tem uma opção!')
+    input('\nPressione um tecla para continuar')
+    print('\nQual? Qual?')
+    input('\nPressione um tecla para continuar')
+    print('\nAtravessar a ponte deformada. Você escolhe'',', personagem_2, '!')
+    print('\nA partir dos 3 cenários apresentados, qual você deseja prosseguir?',personagem_Sereia, ',',personagem_Leão,'ou',personagem_Dragão, '?')
+
+
+# função personagem_3
+def estoriaPersonagem3():
+    # ------------------------ Cena 1 - A caverna ---------------------------
+
+    print('\nHá muitos anos atrás, na floresta do medo, a caverna de Degimord está fechada e o Feiticeiro Tozart, está querendo domina-lá.')
+    input('\nPressione um tecla para continuar.')
+    print('\nO que não esperava encontrar, era a presença de um portal na entrada da caverna. \nPara abrir terá que quebrar com pedras que está no chão.')
+    input('\nPressione um tecla para continuar.')
+    print('\nEstá preparado? Você escolhe!')
+
+    # ------------------------ Cena 2 - Morcegos ---------------------------
+
+    print('\nOs morcegos estão dominando a caverna e o feiticeiro não consegue enchergar nada em sua volta.')
+    input('\nPressione um tecla para continuar.')
+    print('\nCom bastante dificuldade, o feiticeiro acha uma luz no fim da caverna.')
+    input('\nPressione um tecla para continuar.')
+    print('\nTentando achar alguma coisa que possa te ajudar, ouve uma voz dizendo para ir até a luz no fim da caverna.')
+    input('\nPressione um tecla para continuar.')
+    print('\nQuem é você, voz do horizonte?')
+    input('\nPressione um tecla para continuar.')
+    print('\nVoz da salvação, venha até mim. Você escolhe', personagem_3, '!')
+
+    # ------------------------ Cena 3 - Caminho estreito ---------------------------
+
+    print('\nContinuando a missão de sobreviver e conquistar a caverna, o feiticeiro avista um ser imortal. \nPara sobreviver na caverna ele precisa quebrar a pedra de Ascaban.')
+    input('\nPressione um tecla para continuar.')
+    print('\nFeiticeiro você precisará escolher!')
+    input('\nPressione um tecla para continuar.')
+    print('\nVocê decide'',', personagem_3, '.')
+    print('\nA partir dos 3 cenários apresentados, qual você deseja prosseguir?',personagem_Portal, ',',personagem_Voz,'ou',personagem_SerImortal, '?')
+
+
+
+print ('Bem vindo a Forest of fear. Princesa Daiana, Cavaleiro Brulon e Feiticeiro Tozart são os personagens dessa aventura. \nCuidado para não morrer! Vamos começar?')
+
+
+while (personagem <= 0 or personagem >= 4):
+    print ('Escolha seu personagem: \n 1.', personagem_1,  '\n 2.', personagem_2, '\n 3.', personagem_3)
+    personagem = int(input())
+
+    if personagem == 1:
+        print('Você escolheu', personagem_1, 'para essa aventura. O jogo começou!')
+        estoriaPersonagem1()
+        print ('Escolha seu personagem: \n 1.', personagem_Unicornio,  '\n 2.', personagem_Fada, '\n 3.', personagem_Duende)
+        personagensSegundarios = int(input())
+        
+        if personagensSegundarios == 1:
+            print('Game Over. O Unicórnio estava errado e o reino das bruxas estava acordado. \nAo entrar, a Princesa Daiana foi surpreendida com o ácido mortal que pairava sobre o reino.')
+            print('Fim de jogo')
+        if personagensSegundarios == 2:
+            print('Parábens, você venceu. Enfrentou o caminho perigoso e praticou uma boa ação ajudando a fada!')
+            print('Fim de jogo') 
+        if personagensSegundarios == 3: 
+            print('Game Over. Você não escolheu certo e caiu no poço.') 
+            print('Fim de jogo')
+            
+    elif personagem == 2:
+        print('Você escolheu', personagem_2, 'para essa aventura. O jogo começou!')
+        estoriaPersonagem2()
+        print ('Escolha seu personagem: \n 1.', personagem_Sereia,  '\n 2.', personagem_Leão, '\n 3.', personagem_Dragão)
+        personagensSegundarios = int(input())
+        
+        if personagensSegundarios == 1:
+            print('Parábens, você venceu. O leão faminto está sem comer a um ano e morreu. Você encontrou a saída.')
+            print('Fim de jogo')
+        elif personagensSegundarios == 2:
+            print('Game over. Poxa, você escolheu a matar a cobra. Mas, o que não esperava era que o leão te matasse!')
+            print('Fim de jogo')
+        elif personagensSegundarios == 3:
+            print('Game Over. A ponte deformada não foi confiável. Ao passar por ela, você caiu!')
+            print('Fim de jogo')
+
+
+    elif personagem == 3:
+        print('Você escolheu', personagem_3, 'para essa aventura. O jogo começou!')
+        estoriaPersonagem3()
+        print ('Escolha seu personagem: \n 1.', personagem_Portal,  '\n 2.', personagem_Voz, '\n 3.', personagem_SerImortal)
+        personagensSegundarios = int(input())
+        
+        if personagensSegundarios == 1:
+            print('Game Over. Ao escolher quebrar o portal, o feiticeiro foi morto pelo raio de luz fuminante.')
+            print('Fim de jogo')
+
+        elif personagensSegundarios == 2:
+            print('Parabés. A voz levou o feiticeiro a libertação.')
+            print('Fim de jogo')
+
+        elif personagensSegundarios == 3:
+            print('Game Over. Ao apertar a pedra de Ascaban, o feiticeiro foi atingido pelos raios catastroficos.')
+            print('Fim de jogo')
+    else:
+        print('Opção Inválida')
+
